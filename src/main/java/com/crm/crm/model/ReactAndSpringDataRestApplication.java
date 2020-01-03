6f48dcbbfd632223098534b74a52615c02410fee
Adri Forczek <forczekadri@gmail.com>
2020-01-03 21:59:25 +0000

    strips out first React component

--- src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -4,4 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+}
